b75051e646e3d793073f624dd9fdc8541ebe8e8a
Marvin Deutz <marvin.deutz@yahoo.com>
2025-10-09 19:17:35 +0200

    Add animated gradient background to content blocks and remove test file

--- src/components/react/BackgroundGradient.tsx
+++ src/components/react/BackgroundGradient.tsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber";
+import React from "react";
 import { Vector3 } from "three";
 
 import { cn } from "@/utils/cn";
@@ -6,24 +7,40 @@ import { generateGradientUniforms, GRADIENT_SPECS } from "@/utils/gradients";
 
 import { GradientPlane } from "../r3f/GradientPlane";
 
-interface Props extends React.PropsWithChildren {
+interface BaseProps extends React.PropsWithChildren {
   gradientCols?: string[];
   className?: string;
 }
 
-export const BackgroundGradient = ({
-  gradientCols,
-  className,
-  children,
-}: Props) => {
+interface ImagesProps extends BaseProps {
+  contentType: "images";
+  images: {
+    image: string;
+    alt: string;
+  }[];
+}
+
+interface VideoProps extends BaseProps {
+  contentType: "video";
+  video: string;
+}
+
+type Props = ImagesProps | VideoProps;
+
+export const BackgroundGradient = (props: Props) => {
+  const { className, gradientCols, children, contentType } = props;
+
   const gradient =
     gradientCols && gradientCols?.length > 1
       ? generateGradientUniforms(gradientCols)
       : GRADIENT_SPECS["BLUE"].map((color) => new Vector3(...color));
+
+  const numberOfItems = contentType === "images" ? props.images.length : 1;
+
   return (
     <div
       aria-label="Gradient Container"
-      className={cn("relative h-full w-full", className)}
+      className={cn("relative w-full", className)}
     >
       <div
         aria-label="R3F Canvas Wrapper"
@@ -35,7 +52,9 @@ export const BackgroundGradient = ({
       </div>
       <div
         aria-label="Content Wrapper"
-        className="z-2 relative flex h-full items-center justify-between"
+        className={cn(
+          `z-2 relative grid h-full grid-cols-1 items-center gap-8 sm:grid-cols-${Math.min(numberOfItems, 2)} md:grid-cols-${Math.min(numberOfItems, 4)}`,
+        )}
       >
         {children}
       </div>
