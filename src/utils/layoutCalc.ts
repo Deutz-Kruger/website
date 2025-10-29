f9f41328e8b7306f7d0bb683b61d46fce227a5da
Marvin Deutz <marvin.deutz@yahoo.com>
2025-10-29 18:23:35 +0100

    Fix grid layout hopefully for good. Now proper content is the missing link

--- src/utils/layoutCalc.ts
+++ src/utils/layoutCalc.ts
@@ -5,5 +5,20 @@ import { baseBlock } from "@/content/schemas/blockSchemas";
 type Layout = z.infer<typeof baseBlock.shape.layout>;
 
 export const generateGridPosition = (layout: Layout) => {
-  return `grid-column-start: ${layout.position}; grid-column-end: span ${layout.width}`;
+  let aspect = "";
+  switch (layout.width) {
+    case 1:
+      aspect = "aspect-ratio: 3 / 4;";
+      break;
+    case 2:
+      aspect = "aspect-ratio: 16 / 9;";
+      break;
+    case 3:
+      aspect = "aspect-ratio: 19 / 13;";
+      break;
+    case 4:
+      aspect = "aspect-ratio: 32 / 9;";
+  }
+
+  return `grid-column-start: ${layout.position}; grid-column-end: span ${layout.width}; ${aspect}`;
 };
