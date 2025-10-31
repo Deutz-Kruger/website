a56ed4b57359e9299c4d804228a866c307d90767
Marvin Deutz <marvin.deutz@yahoo.com>
2025-10-31 14:14:13 +0100

    Refactor: Case page layout matches design on desktop

--- src/utils/layoutCalc.ts
+++ src/utils/layoutCalc.ts
@@ -5,20 +5,32 @@ import { baseBlock } from "@/content/schemas/blockSchemas";
 type Layout = z.infer<typeof baseBlock.shape.layout>;
 
 export const generateGridPosition = (layout: Layout) => {
+  let margin = "";
   let aspect = "";
   switch (layout.width) {
     case 1:
-      aspect = "aspect-ratio: 3 / 4;";
+      aspect = "aspect-ratio: 8 / 9;";
       break;
     case 2:
       aspect = "aspect-ratio: 16 / 9;";
       break;
     case 3:
-      aspect = "aspect-ratio: 19 / 13;";
+      aspect = "aspect-ratio: 27 / 9;";
       break;
     case 4:
       aspect = "aspect-ratio: 32 / 9;";
   }
 
-  return `grid-column-start: ${layout.position}; grid-column-end: span ${layout.width}; ${aspect}`;
+  if (layout.width === 1) {
+    if (layout.position % 2 === 0) {
+      margin = "margin-left: auto;";
+    } else {
+      margin = "margin-right: auto;";
+    }
+  }
+
+  return {
+    grid: `grid-column-start: ${layout.position}; grid-column-end: span ${layout.width}; ${aspect}`,
+    margin: margin,
+  };
 };
