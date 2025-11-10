e4cdec3df4c9546ccaae124652f06af0a1530edb
Marvin Deutz <marvin.deutz@yahoo.com>
2025-11-10 22:56:03 +0100

    Chore: update auto generated types for frag shader

--- src/shaders/gradient_bg/gradient.frag.d.ts
+++ src/shaders/gradient_bg/gradient.frag.d.ts
@@ -1,50 +1,27 @@
-declare module "@shaders/gradient_bg/gradient.frag" {
-  namespace THREE {
-    export type Vector3 = { x: number; y: number; z: number; isVector3: true };
-    export type Color = { r: number; g: number; b: number; isColor: true };
-  }
+declare module '@shaders/gradient_bg/gradient.frag' {
+    namespace THREE {
+        export type Vector3 = { x: number, y: number, z: number, isVector3: true };
+        export type Color = { r: number, g: number, b: number, isColor: true };
+    }
 
-  const gradient: string;
+    const gradient: string;
 
-  type Uniforms = {
-    uTime: number;
-    uScrollProgress: number;
-    uColourPalette:
-      | [
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-          number,
-        ]
-      | Float32Array
-      | [
-          [number, number, number],
-          [number, number, number],
-          [number, number, number],
-          [number, number, number],
-        ]
-      | [THREE.Vector3, THREE.Vector3, THREE.Vector3, THREE.Vector3]
-      | [THREE.Color, THREE.Color, THREE.Color, THREE.Color];
-    uUvScale: number;
-    uUvDistortionIterations: number;
-    uUvDistortionIntensity: number;
-    uBlurRadius: number;
-    uBlurStrength: number;
-  };
+    type Uniforms = {
+        uTime: number,
+        uScrollProgress: number,
+        uColourPalette: [number, number, number, number, number, number, number, number, number, number, number, number] | Float32Array | [[number, number, number], [number, number, number], [number, number, number], [number, number, number]] | [THREE.Vector3, THREE.Vector3, THREE.Vector3, THREE.Vector3] | [THREE.Color, THREE.Color, THREE.Color, THREE.Color],
+        uUvScale: number,
+        uUvDistortionIterations: number,
+        uUvDistortionIntensity: number,
+        uBlurRadius: number,
+        uBlurStrength: number
+    };
 
-  export {
-    gradient as default,
-    gradient as glsl,
-    gradient,
-    Uniforms,
-    Uniforms as GradientUniforms,
-  };
+    export {
+        gradient as default,
+        gradient as glsl,
+        gradient,
+        Uniforms,
+        Uniforms as GradientUniforms
+    };
 }
